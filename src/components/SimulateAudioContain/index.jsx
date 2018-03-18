615f8fd9508b1156919615c13a9c51647b5a4228
AbrahamJiang <jiangyangjingness@163.com>
2018-03-19 00:01:18 +0800

    去掉private

--- src/components/SimulateAudioContain/index.jsx
+++ src/components/SimulateAudioContain/index.jsx
@@ -38,7 +38,7 @@ class SimulateAudioContain extends Component {
             <p>介绍：</p>
             <p>——html提供audio音乐播放器，但样式简陋，不易更改，通常情况下采用css自行模拟</p>
             <p>描述：</p>
-            <p>1.点击播放／暂停按钮实现相应功能：利用state改变样式；利用play(),pause()实现功能；</p>
+            <p>1.点击播放／暂停按钮实现相应功能：利用state改变样式；利用play(), pause()实现功能；</p>
             <p>2.模拟进度条：利用audio的属性，音乐总时长--duration，目前播放时间--currentTime来计算进度位置；
               利用addEventListener监听timeupdate事件，实时更新进度条；</p>
             <p>3.ref属性：react提供的属性，用于获取真实dom节点，提倡通过箭头函数中的回调函数来获取；</p>
